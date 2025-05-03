6a8f1a96aa11719601262fe50c259674ae8e3e75
mattersen <mattersen@gmail.com>
2025-05-03 14:44:22 -0700

    adding split image and split links methods

--- src/helper_funcs.py
+++ src/helper_funcs.py
@@ -28,3 +28,41 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     matches = re.findall(r'(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)', text)
     return matches
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        image_tuples = extract_markdown_images(node.text)
+        # If there are no images, return this node and continue
+        if not image_tuples:
+            new_nodes.append(node)
+            continue
+        working_text = node.text
+        for tuple in image_tuples:
+            formatted_link_text = f"![{tuple[0]}]({tuple[1]})"
+            sections = working_text.split(formatted_link_text, 1)
+            new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(TextNode(tuple[0], TextType.IMAGE, tuple[1]))
+            working_text = sections[1]
+        if len(working_text) > 0:
+            new_nodes.append(TextNode(working_text, TextType.TEXT))
+    return new_nodes
+
+def split_nodes_links(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        link_tuples = extract_markdown_links(node.text)
+        # If there are no images, return this node and continue
+        if not link_tuples:
+            new_nodes.append(node)
+            continue
+        working_text = node.text
+        for tuple in link_tuples:
+            formatted_link_text = f"[{tuple[0]}]({tuple[1]})"
+            sections = working_text.split(formatted_link_text, 1)
+            new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(TextNode(tuple[0], TextType.LINK, tuple[1]))
+            working_text = sections[1]
+        if len(working_text) > 0:
+            new_nodes.append(TextNode(working_text, TextType.TEXT))
+    return new_nodes
